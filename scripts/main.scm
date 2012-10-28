14279ced411154c487f9330c6a4daa8f80dc44a7
David Thompson <dthompson@member.fsf.org>
2012-10-28 14:52:47 -0400

    Update demo.

--- scripts/main.scm
+++ scripts/main.scm
@@ -20,7 +20,7 @@
   (set-segments! agenda '())
   (clear-bullet-system bullets))
 
-(define (emit-circle x y num-bullets rotate callback)
+(define (emit-circle x y radius num-bullets rotate callback)
   (define bullet-list '())
   (let iterate ((i 0))
     (when (< i num-bullets)
@@ -31,11 +31,29 @@
 	(iterate (1+ i)))))
   (callback bullet-list))
 
+(define (emit-spiral-forever x y rotate-step delay callback)
+  (coroutine
+   (lambda ()
+     (let repeat ((rotate 0))
+       (emit-circle x y 20 8 rotate callback)
+       (wait delay)
+       (repeat (+ rotate rotate-step))))))
+
+(define (stress-test)
+  (let iterate ((i 0))
+    (when (< i max-bullets)
+      (let ((bullet (make-bullet bullets)))
+	(set-bullet-position  bullets bullet (random 800) (random 600))
+	(set-bullet-direction bullets bullet (random 360))
+	(set-bullet-speed     bullets bullet (+ 50 (random 50)))
+	(iterate (1+ i))))))
+
 (define (bullet-stuff bullet-list)
   (coroutine
    (lambda ()
      (for-each
       (lambda (bullet)
+	(set-bullet-sprite bullets bullet 2)
 	(set-bullet-speed bullets bullet 120))
       bullet-list)
      (wait 1)
@@ -51,25 +69,45 @@
 	(set-bullet-angular-velocity bullets bullet 20))
       bullet-list))))
 
-(define (emit-spiral-forever x y rotate-step delay)
+(define (large-bullet bullet-list)
+  (for-each 
+   (lambda (bullet)
+     (set-bullet-sprite bullets bullet 2)
+     (set-bullet-speed bullets bullet 100))
+   bullet-list))
+
+(define (diamond-bullet bullet-list)
+  (for-each 
+   (lambda (bullet)
+     (set-bullet-sprite bullets bullet 1)
+     (set-bullet-speed bullets bullet 100))
+   bullet-list))
+
+(define (chess-hell x y)
+  (define bullet-list '())
   (coroutine
    (lambda ()
-     (let repeat ((rotate 0))
-       (emit-circle x y 8 rotate bullet-stuff)
-       (wait delay)
-       (repeat (+ rotate rotate-step))))))
-
-(define (stress-test)
-  (let iterate ((i 0))
-    (when (< i max-bullets)
-      (let ((bullet (make-bullet bullets)))
-	(set-bullet-position  bullets bullet (random 800) (random 600))
-	(set-bullet-direction bullets bullet (random 360))
-	(set-bullet-speed     bullets bullet (+ 50 (random 50)))
-	(iterate (1+ i))))))
+     (let rows ((row 0))
+       (when (< row 16)
+	 (let ((offset (if (odd? row) 32 0)))
+	   (let columns ((column 0))
+	     (when (< column 8)
+	       (let ((bullet (make-bullet bullets)))
+		 (set-bullet-position bullets bullet (+ x offset (* column 64)) (+ y (* row 32)))
+		 (set-bullet-sprite bullets bullet 2)
+		 (set! bullet-list (cons bullet bullet-list)))
+	       (columns (1+ column)))))
+	 (rows (1+ row))))
+     (wait 1)
+     (for-each
+      (lambda (bullet)
+	(set-bullet-speed bullets bullet 100)
+	(set-bullet-direction bullets bullet (random 360)))
+      bullet-list))))
 
 (game-on-start-hook game (lambda ()
-			   (set-bullet-system-image bullets "data/images/bullet.png")))
+			   (define sprite-sheet (make-sprite-sheet "data/images/bullets.png" 32 32 0 0))
+			   (set-bullet-system-sprite-sheet bullets sprite-sheet)))
 
 (game-on-update-hook game (lambda (dt)
 			    (update-agenda agenda dt)
