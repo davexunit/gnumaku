dbfee9a21a4eddb481f7b0c917f3330890dd767e
David Thompson <dthompson@member.fsf.org>
2012-11-24 16:59:30 -0500

    Update enemy module.

--- module/gnumaku/enemy.scm
+++ module/gnumaku/enemy.scm
@@ -3,23 +3,27 @@
                        set-enemy-direction! enemy-points set-enemy-points! enemy-hitbox
                        enemy-health set-enemy-health! set-enemy-position! set-enemy-hitbox-size!
                        damage-enemy! enemy-alive? enemy-x enemy-y update-enemy! draw-enemy
-                       enemy-move-to))
-(use-modules (srfi srfi-9) (gnumaku core) (gnumaku yield))
+                       enemy-move-to enemy-wait run-enemy-action enemy-bullet-system set-enemy-bullet-system!))
+
+(use-modules (srfi srfi-9) (gnumaku core) (gnumaku scheduler))
 
 (define-record-type Enemy
-  (%make-enemy sprite hitbox health speed direction points)
+  (%make-enemy sprite action hitbox health speed direction points bullets agenda)
   enemy?
   (sprite enemy-sprite)
+  (action enemy-action set-enemy-action!)
   (speed enemy-speed set-enemy-speed!)
   (direction enemy-direction set-enemy-direction!)
   (points enemy-points set-enemy-points!)
   (hitbox enemy-hitbox)
-  (health enemy-health set-enemy-health!))
+  (health enemy-health set-enemy-health!)
+  (bullet-system enemy-bullet-system set-enemy-bullet-system!)
+  (agenda enemy-agenda))
 
-(define (make-enemy image health points)
+(define (make-enemy image action health points)
   (let ((sprite (make-sprite image)))
     (center-sprite-image! sprite)
-    (%make-enemy sprite (make-rect 0 0 0 0) health 0 0 points)))
+    (%make-enemy sprite action (make-rect 0 0 0 0) health 0 0 points #f (make-agenda))))
 
 (define (set-enemy-position! enemy x y)
   (set-sprite-position! (enemy-sprite enemy) x y))
@@ -46,11 +50,19 @@
 (define (%enemy-dy enemy dt)
   (* (enemy-speed enemy) (sin  (enemy-direction enemy)) dt))
 
+(define (enemy-wait enemy delay)
+  (abort-to-prompt 'coroutine-prompt
+                   (lambda (resume)
+                     (add-to-agenda! (enemy-agenda enemy) delay resume))))
+
+(define (run-enemy-action enemy)
+  ((enemy-action enemy) enemy))
+
 (define (update-enemy! enemy dt)
-  (set-enemy-position!
-   enemy
-   (+ (enemy-x enemy) (%enemy-dx enemy dt))
-   (+ (enemy-y enemy) (%enemy-dy enemy dt))))
+  (update-agenda! (enemy-agenda enemy) 1)
+  (set-enemy-position! enemy
+                       (+ (enemy-x enemy) (%enemy-dx enemy dt))
+                       (+ (enemy-y enemy) (%enemy-dy enemy dt))))
 
 (define (draw-enemy enemy)
   (draw-sprite (enemy-sprite enemy)))
@@ -61,5 +73,5 @@
 	  (dy (- y (enemy-y enemy))))
       (set-enemy-direction! enemy (atan dy dx))
       (set-enemy-speed! enemy speed)
-      (wait (/ (sqrt (+ (* dx dx) (* dy dy))) (enemy-speed enemy)))
+      (enemy-wait enemy (/ (sqrt (+ (* dx dx) (* dy dy))) (enemy-speed enemy)))
       (set-enemy-speed! enemy 0))))
