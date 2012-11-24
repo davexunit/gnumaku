fbcd6bc8ed880aeb793622723ff8b4fa77b8c46d
David Thompson <dthompson@member.fsf.org>
2012-11-24 11:35:34 -0500

    Remove yield module.

--- module/gnumaku/yield.scm
+++
@@ -1,9 +0,0 @@
-(define-module (gnumaku yield))
-(use-modules (gnumaku scheduler))
-(export wait wait-scheduler)
-
-(define wait-scheduler (make-agenda))
-
-(define (wait delay)
-  (abort-to-prompt 'coroutine-prompt (lambda (resume)
-				       (add-to-agenda! wait-scheduler delay resume))))
