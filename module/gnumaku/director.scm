0fd617600b3fe8b9fd14620b3deb31366c154c84
David Thompson <dthompson@member.fsf.org>
2012-11-24 09:25:03 -0500

    Add director module (concept lifted from cocos2d) to manage scenes.

---
+++ module/gnumaku/director.scm
@@ -0,0 +1,87 @@
+(define-module (gnumaku director)
+  #:export (director-init director-run director-show-fps director-push-scene director-pop-scene director-replace-scene))
+
+(use-modules (gnumaku core) (gnumaku fps) (gnumaku scene))
+
+(define director-game (make-game))
+(define director-scenes '())
+(define director-fps (make-fps director-game))
+(define director-font #f)
+(define director-show-fps #f)
+
+(define (director-current-scene)
+  (if (null? director-scenes)
+      #f
+      (car director-scenes)))
+
+(define (director-push-scene scene)
+  ;; Stop current scene
+  (when (director-current-scene)
+    (scene-stop (director-current-scene)))
+  ;; Add new scene
+  (set! director-scenes (cons scene director-scenes))
+  ;; Start new scene
+  (scene-start scene))
+
+(define (director-replace-scene scene)
+  ;; Stop current scene
+  (when (director-current-scene)
+    (scene-stop (director-current-scene)))
+  ;; Replace current scene with new one
+  (set! director-scenes (cons scene (cdr director-scenes)))
+  ;; Start new scene
+  (scene-start scene))
+
+(define (director-pop-scene)
+  ;; Stop current scene
+  (when (director-current-scene)
+    (scene-stop (director-current-scene)))
+  ;; Remove scene
+  (set! director-scenes (cdr director-scenes))
+  (when (director-current-scene)
+    (scene-start (director-current-scene))))
+
+(define* (director-init width height #:optional (fullscreen #f))
+  (game-init director-game width height fullscreen))
+
+(define (director-run scene)
+  (director-push-scene scene)
+  (game-run director-game))
+
+(define (director-draw-fps)
+  (font-draw-text director-font 730 575 '(1 1 1 0.7) (string-append "FPS: " (number->string (fps-last-frames director-fps)))))
+
+(game-on-start-hook
+ director-game
+ (lambda ()
+   (set! director-font (make-font "data/fonts/CarroisGothic-Regular.ttf" 18))))
+
+(game-on-update-hook
+ director-game
+ (lambda (dt)
+   (update-fps! director-fps)
+   ;; Update current scene. If the scene stack is empty, exit the game.
+   (if (director-current-scene)
+       ((scene-on-update (director-current-scene)) dt)
+       (game-stop director-game))))
+
+(game-on-draw-hook
+ director-game
+ (lambda ()
+   (when director-show-fps
+     (director-draw-fps))
+   (when (director-current-scene)
+     (draw-scene (director-current-scene)))))
+
+(game-on-key-pressed-hook
+ director-game
+ (lambda (key)
+   (when (director-current-scene)
+     ((scene-on-key-pressed (director-current-scene)) key))))
+
+(game-on-key-released-hook
+ director-game
+ (lambda (key)
+   (when (director-current-scene)
+     ((scene-on-key-released (director-current-scene)) key))))
+
