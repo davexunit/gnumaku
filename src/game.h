208d5e5bb60a9bf47715a255b0f409e40c0a2310
David Thompson <dthompson@member.fsf.org>
2012-12-02 14:42:02 -0500

    Forgot to include header in last commit.

--- src/game.h
+++ src/game.h
@@ -6,6 +6,8 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
 #include <libguile.h>
 
 typedef struct
