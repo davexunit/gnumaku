c33976067db6d55f1832ee3f97e0528d3f740d2a
David Thompson <dthompson@member.fsf.org>
2012-12-02 12:20:36 -0500

    Remove demo specific bullet type definitions from engine.

--- module/gnumaku/bullet-types.scm
+++ module/gnumaku/bullet-types.scm
@@ -1,34 +1,11 @@
 (define-module (gnumaku bullet-types)
-  #:export (set-bullet-type!))
+  #:export (set-bullet-type register-bullet-type))
 (use-modules (gnumaku core))
 
-(define (set-bullet-type! bullet key)
-  "Provides a hash table of procedures that set bullet sprites and hitboxes"
-  (define bullet-types (make-hash-table))
+(define bullet-types (make-hash-table))
 
-  (define (init-bullet-types)
-    (hash-set! bullet-types 'medium-blue 
-	       (lambda (bullet)
-		 (set-bullet-sprite! bullet 0)
-		 (set-bullet-hitbox! bullet -3 -3 6 6)))
+(define (register-bullet-type key proc)
+  (hash-set! bullet-types key proc))
 
-    (hash-set! bullet-types 'small-diamond
-	       (lambda (bullet)
-		 (set-bullet-sprite! bullet 1)
-		 (set-bullet-hitbox! bullet -2 -2 4 4)))
-
-    (hash-set! bullet-types 'large-orange
-	       (lambda (bullet)
-		 (set-bullet-sprite! bullet 2)
-		 (set-bullet-hitbox! bullet -5 -5 10 10)))
-
-    (hash-set! bullet-types 'small-green
-	       (lambda (bullet)
-		 (set-bullet-sprite! bullet 5)
-		 (set-bullet-hitbox! bullet -2 -2 4 4))))
-
-  ;; initalize bullet types the first time this procedure is called
-  (unless (hash-ref bullet-types 'medium-blue)
-    (init-bullet-types))
-
+(define (set-bullet-type bullet key)
   ((hash-ref bullet-types key) bullet))
