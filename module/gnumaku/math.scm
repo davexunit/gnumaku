ab1862524cb7a0ceb6872c981997da39b310b05c
David Thompson <dthompson@member.fsf.org>
2012-12-02 21:53:46 -0500

    Add rad2deg procedure.

--- module/gnumaku/math.scm
+++ module/gnumaku/math.scm
@@ -1,10 +1,13 @@
-(define-module (gnumaku math))
-(export pi deg2rad cos-deg sin-deg)
+(define-module (gnumaku math)
+  #:export (pi deg2rad rad2deg cos-deg sin-deg))
 
 (define pi 3.141592654)
 
 (define (deg2rad angle)
   (* angle (/ pi 180)))
+
+(define (rad2deg angle)
+  (* angle (/ 180 pi)))
 
 (define (cos-deg angle)
   (cos (deg2rad angle)))
