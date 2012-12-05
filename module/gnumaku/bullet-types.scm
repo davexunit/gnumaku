afd08e7f35aa708081c06f9aa3133bd05d75e078
David Thompson <dthompson@member.fsf.org>
2012-12-05 18:55:40 -0500

    Clean up bullet-types module.

--- module/gnumaku/bullet-types.scm
+++ module/gnumaku/bullet-types.scm
@@ -1,11 +1,13 @@
 (define-module (gnumaku bullet-types)
+  #:use-module (gnumaku core)
   #:export (set-bullet-type register-bullet-type))
-(use-modules (gnumaku core))
 
 (define bullet-types (make-hash-table))
 
 (define (register-bullet-type key proc)
+  "Associates a key with a procedure for setting particular bullet properties."
   (hash-set! bullet-types key proc))
 
 (define (set-bullet-type bullet key)
+  "Applies the bullet properties associated with the given key."
   ((hash-ref bullet-types key) bullet))
