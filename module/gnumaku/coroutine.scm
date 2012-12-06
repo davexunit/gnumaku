84329df7d9f47d60e2c3101457918f2e9caee90f
David Thompson <dthompson@member.fsf.org>
2012-12-05 19:02:51 -0500

    Clean up coroutine module.

--- module/gnumaku/coroutine.scm
+++ module/gnumaku/coroutine.scm
@@ -1,22 +1,21 @@
 (define-module (gnumaku coroutine)
   #:export (coroutine))
 
-
 (define (do-coroutine proc)
+  "Creates a procedure that be yield and resume at any point. Used for cooperative multi-threading."
   (define (handler cont callback . args)
     (define (resume . args)
+      ;; Call continuation that resumes the procedure
       (call-with-prompt 'coroutine-prompt
 			(lambda () (apply cont args))
 			handler))
     (when (procedure? callback)
       (apply callback resume args)))
 
-  (call-with-prompt 'coroutine-prompt
-		    (lambda () (proc))
-		    handler))
+  ;; Call procedure
+  (call-with-prompt 'coroutine-prompt (lambda () (proc)) handler))
 
 (define-syntax coroutine
   (syntax-rules ()
     ((coroutine exp ...)
-     (do-coroutine
-      (lambda () exp ...)))))
+     (do-coroutine (lambda () exp ...)))))
