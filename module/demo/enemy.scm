bb73403bb8b47dd2ceee8bdd63e58d4c09add1b1
David Thompson <dthompson@member.fsf.org>
2012-12-08 14:07:13 -0500

    Update demo.

--- module/demo/enemy.scm
+++ module/demo/enemy.scm
@@ -8,8 +8,6 @@
 
 (define-class <enemy> (<actor>)
   (sprite #:accessor sprite #:init-keyword #:sprite #:init-value #f)
-  (speed #:accessor speed #:init-keyword #:speed #:init-value 0)
-  (direction #:accessor direction #:init-keyword #:direction #:init-value 0)
   (points #:accessor points #:init-keyword #:points #:init-value 0)
   (health #:accessor health #:init-keyword #:health #:init-value 0)
   (action #:accessor action #:init-keyword #:action #:init-value (lambda () #f)))
@@ -23,20 +21,8 @@
 (define-method (alive? (enemy <enemy>))
   (> (health enemy) 0))
 
-(define-method (dx (enemy <enemy>))
-  (* (speed enemy) (cos (direction enemy))))
-
-(define-method (dy (enemy <enemy>))
-  (* (speed enemy) (sin  (direction enemy))))
-
 (define (run-enemy-action enemy)
   ((action enemy) enemy))
-
-(define-method (update (enemy <enemy>))
-  (next-method)
-  (set-position enemy
-                (+ (x enemy) (dx enemy))
-                (+ (y enemy) (dy enemy))))
 
 (define-method (%draw (enemy <enemy>))
   (draw-sprite (sprite enemy)))
