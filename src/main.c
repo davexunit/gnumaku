cba53ed197b36e9422f8e67436ba0a2449a1819a
David Thompson <dthompson@member.fsf.org>
2012-12-16 12:05:26 -0500

    Update gnumaku_init to initialize particle system smob.

--- src/main.c
+++ src/main.c
@@ -8,12 +8,12 @@
 #include "font.h"
 #include "audio.h"
 #include "game.h"
+#include "particle_system.h"
 #include "bullet_system.h"
 #include "draw.h"
 
 void
-init_gnumaku ()
-{
+init_gnumaku () {
     // Create smobs so we can call C code from Scheme
     init_transform_type ();
     init_sprite_sheet_type ();
@@ -23,13 +23,13 @@ init_gnumaku ()
     init_font_type ();
     init_sample_type ();
     init_game_type ();
+    init_particle_system_type ();
     init_bullet_system_type ();
     bind_draw_funcs ();
 }
 
 void
-init_gnumaku_module ()
-{
+init_gnumaku_module (void) {
     // Create the gnumaku core module and export all procedures
     scm_c_define_module ("gnumaku core", init_gnumaku, NULL);
 }
