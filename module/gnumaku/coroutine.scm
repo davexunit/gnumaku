34705a27cf1768ff831fe32a45186f264e549a29
David Thompson <dthompson@member.fsf.org>
2012-12-26 17:52:54 -0500

    Remove coroutine procedure and replace it with a much prettier define-coroutine macro.

--- module/gnumaku/coroutine.scm
+++ module/gnumaku/coroutine.scm
@@ -1,24 +1,28 @@
 (define-module (gnumaku coroutine)
   #:use-module (oop goops)
-  #:export (coroutine wait))
+  #:export (define-coroutine wait))
 
 (define (do-coroutine proc)
   "Creates a procedure that be yield and resume at any point. Used for cooperative multi-threading."
   (define (handler cont callback . args)
     (define (resume . args)
-      ;; Call continuation that resumes the procedure
+      ;; Call continuation that resumes the procedure.
       (call-with-prompt 'coroutine-prompt
 			(lambda () (apply cont args))
 			handler))
     (when (procedure? callback)
       (apply callback resume args)))
 
-  ;; Call procedure
-  (call-with-prompt 'coroutine-prompt (lambda () (proc)) handler))
+  ;; Call procedure.
+  (call-with-prompt 'coroutine-prompt proc handler))
 
-(define-syntax coroutine
+;; Creates a procedure that is executed as a coroutine.
+(define-syntax define-coroutine
   (syntax-rules ()
-    ((coroutine exp ...)
-     (do-coroutine (lambda () exp ...)))))
+    ((_ (name ...) . body)
+     (define (name ...) (do-coroutine (lambda () . body))))))
 
+;; Generic method definition for yielding from a coroutine for an amount of time.
+;; This is done here to avoid module import issues that can arise when a generic
+;; is defined in more than once place.
 (define-generic wait)
