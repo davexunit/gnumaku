22ef525f6f4ad248f3bf84e7db638528d851e283
David Thompson <dthompson@member.fsf.org>
2012-12-26 17:36:45 -0500

    Add helpful repeat procedure to gnumaku core module.

--- module/gnumaku/core.scm
+++ module/gnumaku/core.scm
@@ -1,3 +1,11 @@
-(define-module (gnumaku core))
+;; Core C library plus any other useful miscellaneous procedures.
+
+(define-module (gnumaku core)
+  #:export (repeat))
 
 (load-extension "./gnumaku.so" "init_gnumaku")
+
+(define (repeat times proc)
+  (do ((i 1 (1+ i)))
+       ((> i times))
+    (proc i)))
