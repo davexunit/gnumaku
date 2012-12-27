91a5385806063f13c22254d150140d1323c61093
David Thompson <dthompson@member.fsf.org>
2012-12-26 20:12:38 -0500

    Refactor bullet types module to simply keep a hash of BulletType smobs.

--- module/gnumaku/bullet-types.scm
+++ module/gnumaku/bullet-types.scm
@@ -1,13 +1,14 @@
 (define-module (gnumaku bullet-types)
+  #:use-module (srfi srfi-9)
   #:use-module (gnumaku core)
-  #:export (set-bullet-type register-bullet-type))
+  #:export (get-bullet-type register-bullet-type))
 
 (define bullet-types (make-hash-table))
 
-(define (register-bullet-type key proc)
+(define (register-bullet-type key type)
   "Associates a key with a procedure for setting particular bullet properties."
-  (hash-set! bullet-types key proc))
+  (hash-set! bullet-types key type))
 
-(define (set-bullet-type bullet key)
+(define (get-bullet-type key)
   "Applies the bullet properties associated with the given key."
-  ((hash-ref bullet-types key) bullet))
+  (hash-ref bullet-types key))
