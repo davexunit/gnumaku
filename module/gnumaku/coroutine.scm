f0d5e1ae659cb437962f9b310e75efb1c90bf7a0
David Thompson <dthompson@member.fsf.org>
2012-12-27 19:25:13 -0500

    Improve define-coroutine macro to allow recursion without spawning additional coroutines.

--- module/gnumaku/coroutine.scm
+++ module/gnumaku/coroutine.scm
@@ -2,7 +2,7 @@
   #:use-module (oop goops)
   #:export (define-coroutine wait))
 
-(define (do-coroutine proc)
+(define (do-coroutine thunk)
   "Creates a procedure that be yield and resume at any point. Used for cooperative multi-threading."
   (define (handler cont callback . args)
     (define (resume . args)
@@ -14,13 +14,19 @@
       (apply callback resume args)))
 
   ;; Call procedure.
-  (call-with-prompt 'coroutine-prompt proc handler))
+  (call-with-prompt 'coroutine-prompt thunk handler))
 
 ;; Creates a procedure that is executed as a coroutine.
 (define-syntax define-coroutine
   (syntax-rules ()
-    ((_ (name ...) . body)
-     (define (name ...) (do-coroutine (lambda () . body))))))
+    ((_ (name . args) . body)
+     ;; Outer define for the do-coroutine call.
+     (define (name . args)
+       ;; Make a new procedure with the same signature so that a recursive procedure
+       ;; can be created without starting a new coroutine with each call to itself.
+       (define (name . args) . body)
+       ;; Coroutine time.
+       (do-coroutine (lambda () (name . args)))))))
 
 ;; Generic method definition for yielding from a coroutine for an amount of time.
 ;; This is done here to avoid module import issues that can arise when a generic
