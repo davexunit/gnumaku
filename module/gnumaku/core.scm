da6dcb6b339ee4bcbb291a9708420cb8b1691b18
David Thompson <dthompson@member.fsf.org>
2012-12-29 23:51:21 -0500

    Update make-sprite to accept an optional #:color keyword argument.

--- module/gnumaku/core.scm
+++ module/gnumaku/core.scm
@@ -6,11 +6,13 @@
 (load-extension "./gnumaku.so" "init_gnumaku")
 
 (define (repeat times proc)
+  "Do something a bunch of times."
   (do ((i 1 (1+ i)))
        ((> i times))
     (proc i)))
 
 (define* (make-sprite image #:optional #:key (position (make-vector2 0 0))
-                      (scale (make-vector2 1 1)) (rotation 0) (anchor #f))
+                      (scale (make-vector2 1 1)) (rotation 0)
+                      (color (make-color-f 1 1 1 1)) (anchor #f))
   "Helpful wrapper around %make-sprite primitive procedure."
-  (%make-sprite image position scale rotation anchor))
+  (%make-sprite image position scale rotation color anchor))
