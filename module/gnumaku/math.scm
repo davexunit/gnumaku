ccab810e8b7c7aa15e266274f617c9228c5605bb
David Thompson <dthompson@member.fsf.org>
2013-01-01 11:09:06 -0500

    Add atan-deg procedure that returns arc tangent in degrees.

--- module/gnumaku/math.scm
+++ module/gnumaku/math.scm
@@ -1,5 +1,5 @@
 (define-module (gnumaku math)
-  #:export (pi deg2rad rad2deg cos-deg sin-deg))
+  #:export (pi deg2rad rad2deg cos-deg sin-deg atan-deg))
 
 (define pi 3.141592654)
 
@@ -14,3 +14,6 @@
 
 (define (sin-deg angle)
   (sin (deg2rad angle)))
+
+(define (atan-deg y x)
+  (rad2deg (atan y x)))
