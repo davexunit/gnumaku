0523ac921f13951e42fdd7e090a4d3daf7763783
David Thompson <dthompson@member.fsf.org>
2013-01-05 00:07:08 -0500

    Update bullet type C module to use new coding conventions.

--- src/bullet_type.c
+++ src/bullet_type.c
@@ -1,73 +1,60 @@
 #include "bullet_type.h"
 
 static scm_t_bits bullet_type_tag;
-SCM_SYMBOL (sym_blend_alpha, "alpha");
-SCM_SYMBOL (sym_blend_add, "add");
 
-BlendMode
-scm_to_blend_mode (SCM blend_mode) {
-    if (scm_is_true (scm_eq_p (blend_mode, sym_blend_alpha))) {
-        return BLEND_ALPHA;
-    } else if (scm_is_true (scm_eq_p (blend_mode, sym_blend_add))) {
-        return BLEND_ADD;
-    }
-
-    /* Standard alpha blending by default. */
-    return BLEND_ALPHA;
-}
-
-BulletType*
-check_bullet_type (SCM bullet_type_smob) {
+GmkBulletType *
+gmk_check_bullet_type (SCM bullet_type_smob)
+{
     scm_assert_smob_type (bullet_type_tag, bullet_type_smob);
 
-    return (BulletType *) SCM_SMOB_DATA (bullet_type_smob);
+    return (GmkBulletType *) SCM_SMOB_DATA (bullet_type_smob);
 }
 
-SCM_DEFINE (make_bullet_type, "make-bullet-type", 4, 0, 0,
+SCM_DEFINE (gmk_make_bullet_type, "make-bullet-type", 4, 0, 0,
             (SCM s_image, SCM s_hitbox, SCM s_blend_mode, SCM s_directional),
             "Make a new bullet type instance.")
 {
-    SCM smob;
-    BulletType *bullet_type;
+    GmkBulletType *bullet_type;
     int image = scm_to_int (s_image);
     Rect hitbox = scm_to_rect (s_hitbox);
-    BlendMode blend_mode = scm_to_blend_mode (s_blend_mode);
+    GmkBlendMode blend_mode = gmk_scm_to_blend_mode (s_blend_mode);
     bool directional = scm_to_bool (s_directional);
 
-    bullet_type = (BulletType *) scm_gc_malloc (sizeof (BulletType), "bullet_type");
+    bullet_type = (GmkBulletType *) scm_gc_malloc (sizeof (GmkBulletType), "bullet_type");
     bullet_type->image = image;
     bullet_type->hitbox = hitbox;
     bullet_type->blend_mode = blend_mode;
     bullet_type->directional = directional;
-    SCM_NEWSMOB (smob, bullet_type_tag, bullet_type);
-
-    return smob;
+    SCM_RETURN_NEWSMOB (bullet_type_tag, bullet_type);
 }
 
 static size_t
-free_bullet_type (SCM bullet_type_smob) {
-    BulletType *bullet_type = (BulletType *) SCM_SMOB_DATA (bullet_type_smob);
+free_bullet_type (SCM bullet_type_smob)
+{
+    GmkBulletType *bullet_type = (GmkBulletType *) SCM_SMOB_DATA (bullet_type_smob);
 
-    scm_gc_free (bullet_type, sizeof (BulletType), "bullet_type");
+    scm_gc_free (bullet_type, sizeof (GmkBulletType), "bullet_type");
 
     return 0;
 }
 
 static int
-print_bullet_type (SCM bullet_type_smob, SCM port, scm_print_state *pstate) {
+print_bullet_type (SCM bullet_type_smob, SCM port, scm_print_state *pstate)
+{
     scm_puts ("#<bullet-type>", port);
 
     return 1;
 }
 
 void
-init_bullet_type_type (void) {
-    bullet_type_tag = scm_make_smob_type ("<bullet-type>", sizeof (BulletType));
+gmk_init_bullet_type (void)
+{
+    bullet_type_tag = scm_make_smob_type ("<bullet-type>", sizeof (GmkBulletType));
     scm_set_smob_mark (bullet_type_tag, 0);
     scm_set_smob_free (bullet_type_tag, free_bullet_type);
     scm_set_smob_print (bullet_type_tag, print_bullet_type);
 
 #include "bullet_type.x"
 
-    scm_c_export (s_make_bullet_type, NULL);
+    scm_c_export (s_gmk_make_bullet_type, NULL);
 }
--- src/bullet_type.h
+++ src/bullet_type.h
@@ -1,5 +1,5 @@
-#ifndef BULLET_TYPE_H
-#define BULLET_TYPE_H
+#ifndef GMK_BULLET_TYPE_H
+#define GMK_BULLET_TYPE_H
 
 #include <allegro5/allegro.h>
 #include <libguile.h>
@@ -9,18 +9,21 @@
 /*
  * BulletType
  *
- * Common properties for bullets of the same type.
+ * Common set of properties for bullets.
  */
 typedef struct {
     bool directional;
     int image;
     Rect hitbox;
-    BlendMode blend_mode;
-} BulletType;
+    GmkBlendMode blend_mode;
+} GmkBulletType;
 
-BlendMode scm_to_blend_mode (SCM blend_mode);
-BulletType* check_bullet_type (SCM bullet_type_smob);
-/* Well, this name sure turned out silly. */
-void init_bullet_type_type (void);
+GmkBulletType *gmk_check_bullet_type (SCM bullet_type_smob);
+
+SCM gmk_make_bullet_type (SCM s_image, SCM s_hitbox, SCM s_blend_mode,
+                          SCM s_directional);
+
+    
+void gmk_init_bullet_type (void);
 
 #endif
