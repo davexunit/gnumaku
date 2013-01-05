3bd1603a26a8b38c6c446e0179f835b9e37f9569
David Thompson <dthompson@member.fsf.org>
2013-01-05 13:08:55 -0500

    Formatting.

--- src/image.h
+++ src/image.h
@@ -3,7 +3,7 @@
 
 #include "common.h"
 
-/* A wrapper for ALLEGRO_BITMAP */
+/* A wrapper for ALLEGRO_BITMAP. */
 typedef struct {
     ALLEGRO_BITMAP *bitmap;
 } GmkImage;
@@ -13,9 +13,8 @@ SCM gmk_scm_from_bitmap (ALLEGRO_BITMAP *bitmap);
 SCM gmk_load_image (SCM filename);
 SCM gmk_image_width (SCM image);
 SCM gmk_image_height (SCM image);
-SCM gmk_draw_image(SCM image, SCM x, SCM y);
+SCM gmk_draw_image (SCM image, SCM x, SCM y);
 SCM gmk_set_render_image (SCM image);
-
 void gmk_init_image (void);
 
 #endif
