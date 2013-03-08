6d8c6786aa69333fec6eac70aefdac389746c2ca
David Thompson <dthompson@member.fsf.org>
2013-03-08 12:58:26 -0500

    Add scene module.

---
+++ gnumaku/scene.scm
@@ -0,0 +1,19 @@
+(define-module (gnumaku scene)
+  #:use-module (oop goops)
+  #:export (draw
+            update
+            on-start
+            on-stop
+            on-pause
+            on-resume
+            on-key-pressed
+            on-key-released))
+
+(define-generic draw)
+(define-generic update)
+(define-generic on-start)
+(define-generic on-stop)
+(define-generic on-pause)
+(define-generic on-resume)
+(define-generic on-key-released)
+(define-generic on-key-released)
