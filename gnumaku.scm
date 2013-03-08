7e92fb30410b1979788dedaff768d48f3d1988f8
David Thompson <dthompson@member.fsf.org>
2013-03-08 12:59:14 -0500

    Start game with shooter scene.

--- gnumaku.scm
+++ gnumaku.scm
@@ -2,7 +2,14 @@
 !#
 
 ;; Just a simple bootstrap script.
-
 (add-to-load-path ".")
-(use-modules (gnumaku main))
-(gmk-main)
+(use-modules (system repl server)
+             (gnumaku director)
+             (gnumaku scenes shooter))
+
+;; Create REPL server for live-coding awesomeness.
+(spawn-server)
+
+;; Start game.
+(director-init)
+(director-run (make-shooter-scene))
