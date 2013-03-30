0148f918002a92ad32d90563a26941df734cfe5b
David Thompson <dthompson@member.fsf.org>
2013-03-30 11:41:16 -0400

    Remove old module directory with outdated, unused code.

--- module/gnumaku/generics.scm
+++
@@ -1,7 +0,0 @@
-;; This module is necessary because I can't resolve GOOPS generic method duplications in a satisfactory way.
-(define-module (gnumaku generics)
-  #:use-module (oop goops)
-  #:export (update draw))
-
-(define-generic draw)
-(define-generic update)
